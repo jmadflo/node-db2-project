0aa2528da508b672296091641082e26e4793ebdf
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-12 14:42:13 -0700

    added table to a new database using knex migrations

---
+++ migrations/20200512142009_create-cars-table.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+    // Table records should always have id which autoincrements, VIN, Make, Model, and Mileage
+    // Records may also have Transmission Type and Status of the Title which are both optional
+    return knex.schema.createTable('cars', table => {
+        table.increments()
+        table.text('VIN', 17)
+            .unique()
+            .notNullable()
+        table.text('Make', 50)
+            .notNullable()
+        table.text('Model', 50)
+            .notNullable()
+        table.decimal('Mileage')
+            .notNullable()
+        table.text('Transmission Type', 30)
+        table.text('Status of Title', 100)
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('cars')
+};
