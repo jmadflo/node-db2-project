d88d8f25ca1f59ff7f56cb4df413b8c7913f88d5
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-12 17:05:51 -0700

    added seed data

--- seeds/01-cars.js
+++ seeds/01-cars.js
@@ -6,8 +6,9 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('cars').insert([
         { Make: 'Tesla', Model: 'Model S', VIN: '1234567890abcdefg', Mileage: 100 },
-        { Make: 'Tesla', Model: 'Model 3', VIN: '2234567890abcdefg', Mileage: 80 },
-        { Make: 'Tesla', Model: 'Model X', VIN: '3234567890abcdefg', Mileage: 120, Transmission_Type: 'Electric', Status_of_Title: 'New' },
+        { Make: 'Tesla', Model: 'Model S', VIN: '2234567890abcdefg', Mileage: 100, Transmission_Type: 'Electric', Status_of_Title: 'New' }
+        { Make: 'Tesla', Model: 'Model 3', VIN: '3234567890abcdefg', Mileage: 80 },
+        { Make: 'Tesla', Model: 'Model X', VIN: '4234567890abcdefg', Mileage: 120, Transmission_Type: 'Electric', Status_of_Title: 'New' },
       ]);
     });
 };
